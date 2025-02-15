11bd65f504734501b36a1b1f0794f3b43d0749b5
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-15 16:23:11 +0530

    add ul,li

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <section className="bg-green-600">
+    <section className="bg-green-600 min-h-screen">
       section
     </section>
    </main>
