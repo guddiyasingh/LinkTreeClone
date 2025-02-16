2dfa831fd9c3a4a901e40bd43dee4ea7f862b15e
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-17 02:11:19 +0530

    add buttons

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main>
-    <section className="bg-green-600 min-h-screen">
+    <section className="bg-[#254f1a] min-h-screen">
       section
     </section>
    </main>
