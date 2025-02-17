dae839227f1ad8aca420249ca0a18166562ccd1e
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-18 04:25:35 +0530

    some changes in input and buttons of app/page.js

--- app/page.js
+++ app/page.js
@@ -5,14 +5,20 @@ export default function Home() {
    <main>
     <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
 
-        <div className=" flex items-center justify-center flex-col gap-4 ml-[10vw]">
+        <div className=" flex justify-center flex-col gap-4 ml-[10vw] gap-3">
 
-         <p className="text-yellow-300 font-bold text-5xl"> Everything you are. In one, simple link in bio.</p>
+         <p className="text-yellow-300 font-bold text-5xl"> Everything you </p>
+         <p className="text-yellow-300 font-bold text-5xl">are. In one,</p>
+         <p className="text-yellow-300 font-bold text-5xl"> simple link in bio.</p>
          <p className="text-yellow-300 text-xl">Join 50M+ people using Linktree for their link in bio. 
           One link to help you share everything you create, curate and sell from your
        Instagram,TikTok, Twitter, YouTube and other social media profiles.</p>
+       <div className="input flex gap-3">
+            <input className="px-1 py-1 rounded-md focus:outline-green-800" type="text" placeholder="linktr.ee/your-url"/>
+           <button className="bg-pink-300 rounded-full px-4 py-4 font-semibold">Claim your Bittree</button>
+       </div>
      </div>
-     <div className="flex items-center justify-center flex-col mr-[10vw]">
+     <div className="flex items-center justify-center flex-col mr-[10vw] mt-[6vw] ">
       <img src="/img.png" alt="homepage image "/>
      </div>
     </section>
