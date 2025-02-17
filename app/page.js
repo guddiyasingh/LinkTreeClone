99341b0df189e1091b5c0b6152aaa69ec5c74b25
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-17 19:02:19 +0530

    some chnages in page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
    <main>
     <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
-     <div className="bg-red-200">
+      
+     <div className="bg-red-200 flex items-center justify-center">
+
          <p className="text-yellow-300 font-bold text-3x1"> Everything you are. In one, simple link in bio.</p>
          <p>Join 50M+ people using Linktree for their link in bio. 
           One link to help you share everything you create, curate and sell from your
