10882fa188bb37622a608f1efb7cf08c93d01315
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-18 03:13:18 +0530

    add images

--- app/page.js
+++ app/page.js
@@ -4,16 +4,16 @@ export default function Home() {
   return (
    <main>
     <section className="bg-[#254f1a] min-h-[100vh] grid grid-cols-2">
-      
-     <div className="bg-red-200 flex items-center justify-center">
 
-         <p className="text-yellow-300 font-bold text-3x1"> Everything you are. In one, simple link in bio.</p>
-         <p>Join 50M+ people using Linktree for their link in bio. 
+        <div className=" flex items-center justify-center flex-col gap-4 ml-[10vw]">
+
+         <p className="text-yellow-300 font-bold text-5xl"> Everything you are. In one, simple link in bio.</p>
+         <p className="text-yellow-300 text-xl">Join 50M+ people using Linktree for their link in bio. 
           One link to help you share everything you create, curate and sell from your
        Instagram,TikTok, Twitter, YouTube and other social media profiles.</p>
      </div>
-     <div>
-      your column 2
+     <div className="flex items-center justify-center flex-col mr-[10vw]">
+      <img src="/img.png" alt="homepage image "/>
      </div>
     </section>
     <section className="bg-red-700 min-h-[100vh]">
